db4cab2f209374d314d64727b60a17c01d46ba93
moexplore <mrubered@yahoo.com>
2021-06-09 13:55:49 -0500

    first commit

---
+++ practice.js
@@ -0,0 +1,42 @@
+// setName()
+// function setName(){
+//     var name = 'Moshe'
+//     console.log(name)
+
+// }
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ["apple", "pear", "date"];
+
+// function printFruit() {
+//   let favFruit = fruits[2];
+//   console.log(fruits[0]);
+//   function printFruit2() {
+//     let leastfav = "prune";
+//     console.log(favFruit);
+//   };
+
+//   console.log(leastfav)
+//   printFruit2()
+// }
+
+// printFruit();
+// nameo()
+// function nameo() {
+//     console.log("Hello Moshe")
+// }
+// Worked for nameo but not when I put it in a variable.  This config prevents the function from being hoisted?
+//alert()
+let alerting = function(name) {
+    alert("It's an aloit " + name )
+}
+alerting('Moshe')
+console.log(alert)
